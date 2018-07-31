79f549bd63c6d7e62f5ccf07df22fd19d38d672f
Divya Madhuri <alaganduladivyamadhuri@gmail.com>
2018-07-31 11:59:10 +0530

    import functionality directories and sub directories

---
+++ Import_Functionality/VirtualEnv/Import_Operator_Classes.py
@@ -0,0 +1,9 @@
+from Class_Direct.Class_Addition import Addition
+from Class_Direct.Class_Substraction import Subtraction
+
+print('Now I am in new operator classes python file')
+
+Add1 = Addition(10, 12, 34)
+Sub1 = Subtraction(23,12)
+
+print('Finished calculation, it means that import worked successfully')
