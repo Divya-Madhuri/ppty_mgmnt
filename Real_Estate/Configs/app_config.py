2f9522a7085e1bdd327413a0fd8eb0f228a4c0ff
Divya Madhuri <alaganduladivyamadhuri@gmail.com>
2018-08-31 20:56:16 +0530

    add config file and templates folder

--- Real_Estate/Configs/app_config.py
+++ Real_Estate/Configs/app_config.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/divyamadhuri/Documents/Python/ppty_mgmnt/RE_Mgmt/REM_DB.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/divyamadhuri/Documents/Python/ppty_mgmnt/Real_Estate/RESdb.db'
+app.config['SECRET_KEY'] = "random string"
 
 
 db = SQLAlchemy(app)
