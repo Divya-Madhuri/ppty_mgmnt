fb51ff596fd0ea58353fd63d1421dd4cb1ea9789
Divya Madhuri <alaganduladivyamadhuri@gmail.com>
2018-09-01 19:47:20 +0530

    add templates and static folder

--- Real_Estate/Configs/app_config.py
+++ Real_Estate/Configs/app_config.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from sqlite3 import Connection as SQLite3Connection
 
 app = Flask(__name__)
 
@@ -8,3 +11,11 @@ app.config['SECRET_KEY'] = "random string"
 
 
 db = SQLAlchemy(app)
+
+
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
