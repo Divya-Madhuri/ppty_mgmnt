a3193a7f237e142442c7ee35bfdc856d91e4f456
Divya Madhuri <alaganduladivyamadhuri@gmail.com>
2018-09-01 19:44:44 +0530

    add new asset and new customer form

--- Real_Estate/Asset_Form.py
+++ Real_Estate/Asset_Form.py
@@ -1,9 +1,13 @@
 from Configs.app_config import app, db
 from flask import render_template, request, flash
 from Classes.Asset_Class import AssetsData
+from Classes.People_Class import People
+from Classes.Roles import Roles
 
 
 All_Assets=AssetsData.query.all()
+customer_name= AssetsData.query.filter_by(name = 'test').all()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
@@ -35,8 +39,24 @@ def add_assets():
 
 @app.route('/All', methods=['GET', 'POST'])
 def all_assets():
-   # return AssetsData.query.all()
     return render_template('AllAsset.html', All_Assets=All_Assets)
+
+
+@app.route('/add_people', methods=['GET', 'POST'])
+def add_people():
+
+    if request.method == 'POST':
+        if not request.form['id'] or not request.form['name']:
+            flash('Please enter all the fields', 'error')
+        else:
+            people = People(request.form['id'], request.form['name'], request.form['role'],
+                                request.form['gender'], request.form['age'], request.form['mobile_num'],
+                                request.form['alternate_num'], request.form['address'])
+
+            db.session.add(people)
+            db.session.commit()
+            flash('Record was successfully added')
+    return render_template('AddPeople.html')
 
 
 if __name__ == '__main__':
