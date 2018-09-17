4cd35a8de0fd46d9a02deb891e95e3a54a3c3ec3
Divya Madhuri <alaganduladivyamadhuri@gmail.com>
2018-09-17 10:16:33 +0530

    Add Update functionality, back functionality

--- Real_Estate/Asset_Form.py
+++ Real_Estate/Asset_Form.py
@@ -1,5 +1,5 @@
 from Configs.app_config import app, db
-from flask import render_template, request, flash
+from flask import render_template, request, flash, redirect
 from Classes.Asset_Class import AssetsData
 from Classes.People_Class import People
 from Classes.Roles import Roles
@@ -14,12 +14,26 @@ def welcome():
     return render_template('welcome.html')
 
 
+@app.route('/All', methods=['GET', 'POST'])
+def all_assets():
+    return render_template('AllAsset.html', All_Assets=All_Assets)
+
+
+@app.route('/Back', methods=['GET', 'POST'])
+def back():
+    return render_template('welcome.html')
+
+
+@app.route('/BackAsset', methods=['GET', 'POST'])
+def backasset():
+    return render_template('AllAsset.html', All_Assets=All_Assets)
+
+
 @app.route('/Add', methods=['GET', 'POST'])
 def add_assets():
-    #return render_template('AddAsset.html')
-    #Asset = AssetsData(2, 'Name Two ', ' Loc', ' dime',1, ' Status',789, 987, ' mode', 4, 'Some charge')
-    #db.session.add(Asset)
-    #db.session.commit()
+
+    if request.form:
+        print(request.form)
 
     if request.method == 'POST':
         if not request.form['id'] or not request.form['name'] or not request.form['location']:
@@ -37,11 +51,6 @@ def add_assets():
     return render_template('AddAsset.html')
 
 
-@app.route('/All', methods=['GET', 'POST'])
-def all_assets():
-    return render_template('AllAsset.html', All_Assets=All_Assets)
-
-
 @app.route('/add_people', methods=['GET', 'POST'])
 def add_people():
 
@@ -57,6 +66,39 @@ def add_people():
             db.session.commit()
             flash('Record was successfully added')
     return render_template('AddPeople.html')
+
+
+@app.route('/update/<update_id>', methods=['GET', 'POST'])
+def update(update_id):
+
+    update_asset = AssetsData.query.filter_by(id=update_id).first()
+
+    if request.method == 'GET' :
+        return render_template('UpdateAsset.html', update_id=update_id, update_asset=update_asset)
+
+    elif request.method == 'POST':
+        if not request.form['id'] or not request.form['name']:
+            flash('Please enter all the fields', 'error')
+        else:
+            update_assets = AssetsData(request.form['id'], request.form['name'],
+                                request.form['location'], request.form['dimensions'], request.form['history_id'],
+                                request.form['status'],request.form['Actual_Value'], request.form['Selling_price'],
+                                request.form['Payment_mode'], request.form['Pay_duration'],
+                                request.form['Broker_charges'])
+            update_asset.name = update_assets.name
+            update_asset.location = update_assets.location
+            update_asset.dimensions = update_assets.dimensions
+            update_asset.status = update_assets.status
+            update_asset.Actual_Value = update_assets.Actual_Value
+            update_asset.Selling_price = update_assets.Selling_price
+            update_asset.Payment_mode = update_assets.Payment_mode
+            update_asset.Pay_duration = update_assets.Pay_duration
+            update_asset.Broker_charges = update_assets.Broker_charges
+
+            db.session.commit()
+            flash('Record was successfully updated')
+
+    return redirect("All")
 
 
 if __name__ == '__main__':
