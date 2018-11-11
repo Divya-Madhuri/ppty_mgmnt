8cd968c911883500c2a985068436392507044420
Divya Madhuri <alaganduladivyamadhuri@gmail.com>
2018-11-11 12:55:31 +0530

    remove Import Funct files as moved to archive folder

--- Import_Functionality/Class_Direct/Class_Addition.py
+++
@@ -1,9 +0,0 @@
-class Addition:
-    print('Test Addition Class')
-
-    def __init__(self, x, y, z):
-
-        self.x = x
-        self.y = y
-        self.z = z
-        print('This is Addition function and result of your numbers is : ', x+y+z)
